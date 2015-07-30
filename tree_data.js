258b9b9e30dd7cc5fb46f89210e18dd2cc3595af
unknown <shadc@IT-16616.deschutes.org>
2015-07-30 14:20:50 -0700

    initial commit

---
+++ tree_data.js
@@ -0,0 +1,87 @@
+var appsData = [{
+    "name": "Applications Team",
+    "parent": "null",
+    "_children": [{
+        "name": "COTS Support",
+        "_children": [{
+            "name": "RFP/RFQ"
+        }, {
+            "name": "Technical Guidance"
+        }, {
+            "name": "Project Management"
+        }, {
+            "name": "Data Conversion"
+        }, {
+            "name": "Training/Support"
+        }, {
+            "name": "Maintenance/Updates"
+        }]
+    }, {
+        "name": "Application Development",
+        "_children": [{
+            "name": "Desktop Solutions",
+            "url": "#/11/1"
+        }, {
+            "name": "Web Solutions",
+            "url": "#/11/2"
+        }]
+    }, {
+        "name": "Hybrid",
+        "_children": [ {
+            "name": "SharePoint"
+        }, {
+            "name": "Laserfiche"
+        }, {
+            "name": "Middleware/Scripting"
+        }, {
+            "name": "GIS",
+            "url": "#/11/3"
+    }
+        ]
+    }, {
+        "name": "GIS Data maintenance",
+        "_children": [{
+            "name": "Low demand departements"
+        }]
+    }
+    ]
+}];
+
+
+var orgData = [{
+    "name": "I.T.",
+    "parent": "null",
+    "children": [{
+        "name": "Applications Team",
+        "children": [{
+            "name": "Senior Systems Analyst (Web)",
+            "user": "Eric"
+        }, {
+            "name": "Systems Analyst II (Database)",
+            "user": "Toby"
+        }, {
+            "name": "Systems Analyst II (Applications)",
+            "user": "Deb"
+        }, {
+            "name": "Systems Analyst I (Developer)",
+            "user": "Jim"
+        }, {
+            "name": "GIS Analyst/Programmer",
+            "user": "Melinda"
+        }, {
+            "name": "GIS Analyst",
+            "user": "Geri"
+        }, {
+            "name": "Senior Programmer/Analyst (Legacy)",
+            "user": "Ray"
+        }, {
+            "name": "Programmer/Analyst (Legacy)",
+            "user": "Diana"
+        }]
+    }, {
+        "name": "Systems Team",
+        "_children": [{
+            "name": "7 x FTE"
+        }]
+    }]
+}];
