3b1d5b172934d9ddc6c4cf909a24ac81607fc03b
Shad Campbell <shadc@gmx.com>
2016-07-13 22:36:00 -0700

    Add OAGITM16 presentation

---
+++ tree_data_oagitm16.js
@@ -0,0 +1,28 @@
+var gisData = [{
+    "name": "GIS @ Deschutes",
+    "parent": "null",
+    "_children": [
+    {
+        "name": "Assessor - 4 Cartographers",
+
+    },
+    {
+        "name": "911 - GIS Analyst",
+   
+    },
+    {
+        "name": "Community Development - GIS Analyst/Programmer",
+     
+    },
+    {
+        "name": "Road Deptartment - GIS Analyst/Programmer",
+    
+    },
+    {
+        "name": "Information Technology - GIS Analyst & GIS Analyst/Programmer",
+ 
+    }
+
+    ]
+}];
+
